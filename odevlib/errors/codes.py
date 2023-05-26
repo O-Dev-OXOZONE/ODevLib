f85c76732e2644374ecd1e52a94cd6cbf1168cd6
IamMaxim <g.maxim.stepanoff@gmail.com>
2023-05-26 12:53:44 +0300

    Updated error codes

--- odevlib/errors/codes.py
+++ odevlib/errors/codes.py
@@ -1,15 +1,22 @@
 # For cases when other instances have relation on this one and their on_delete is set to PROTECTED.
 protected_instance: int = 1
+
 # For cases when exception was not gracefully handled.
 unhandled_error: int = 2
+
 # For permission denials
 permission_denied: int = 3
+
 # When internal database inconsistencies are detected
 internal_data_inconsistency: int = 4
+
 # When requested object does not exist
 does_not_exist: int = 5
+not_found: int = 6
+
 # When data supplied from client is invalid
 invalid_request_data: int = 6
+
 # When some internal error was detected and gracefully handled
 internal_server_error: int = 7
 
@@ -18,5 +25,6 @@ status_code_mapping = {
     invalid_request_data: 400,
     permission_denied: 403,
     does_not_exist: 404,
+    not_found: 404,
     internal_server_error: 500,
 }
