59924a2560d0fc8c03743a6fe881af1bda7da4dd
Maxim Stepanov <g.maxim.stepanoff@gmail.com>
2023-08-16 16:03:31 +0300

    Added unauthenticated error code

--- odevlib/errors/codes.py
+++ odevlib/errors/codes.py
@@ -1,4 +1,7 @@
 # For cases when other instances have relation on this one and their on_delete is set to PROTECTED.
+from rest_framework import status
+
+
 protected_instance: int = 1
 
 # For cases when exception was not gracefully handled.
@@ -20,11 +23,15 @@ invalid_request_data: int = 6
 # When some internal error was detected and gracefully handled
 internal_server_error: int = 7
 
+# When no authentication information was provided
+unauthenticated: int = 8
+
 # Custom HTTP status codes for internal error codes
 status_code_mapping = {
-    invalid_request_data: 400,
-    permission_denied: 403,
-    does_not_exist: 404,
-    not_found: 404,
-    internal_server_error: 500,
+    invalid_request_data: status.HTTP_400_BAD_REQUEST,
+    permission_denied: status.HTTP_403_FORBIDDEN,
+    does_not_exist: status.HTTP_404_NOT_FOUND,
+    not_found: status.HTTP_404_NOT_FOUND,
+    internal_server_error: status.HTTP_500_INTERNAL_SERVER_ERROR,
+    unauthenticated: status.HTTP_401_UNAUTHORIZED,
 }
