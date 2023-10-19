3b8e1ac11f4b9f74577fb518fe357fbf2ced9c79
Maxim Stepanov <g.maxim.stepanoff@gmail.com>
2023-10-19 14:32:04 +0300

    Added synchronous Redis message broker

--- odevlib/distributed/message_broker.py
+++ odevlib/distributed/message_broker.py
@@ -1,8 +1,10 @@
 import abc
 import logging
 from collections.abc import AsyncIterator
+from typing import Iterator
 
-from redis.asyncio.client import Redis
+from redis.asyncio.client import Redis as AIORedis
+from redis import Redis as SyncRedis
 
 
 class MessageBroker(abc.ABC):
@@ -38,9 +40,9 @@ class RedisMessageBroker(MessageBroker):
     even after worker downtime.
     """
 
-    redis_client: Redis
+    redis_client: AIORedis
 
-    def __init__(self, redis_pool: Redis) -> None:
+    def __init__(self, redis_pool: AIORedis) -> None:
         self.redis_client = redis_pool
 
     async def publish(self, channel: str, message: str) -> None:
@@ -62,7 +64,48 @@ class RedisMessageBroker(MessageBroker):
                     stream_id = e[0].decode()
 
                     if b"message" not in e[1]:
-                        logging.warning("Malfored message, skipping")
+                        logging.warning("Malformed message, skipping")
+                        continue
+
+                    message = e[1][b"message"].decode()
+                    yield (stream_id, message)
+
+
+
+class RedisSyncMessageBroker(MessageBroker):
+    """
+    Implementation of MessageBroker using Redis.
+
+    Uses xadd/xread commands to publish/subscribe,
+    plus allows to specify last_id to get new messages
+    even after worker downtime.
+    """
+
+    redis_client: SyncRedis
+
+    def __init__(self, redis_pool: SyncRedis) -> None:
+        self.redis_client = redis_pool
+
+    def publish(self, channel: str, message: str) -> None:
+        self.redis_client.xadd(channel, {"message": message})
+
+    def asubscribe(
+        self,
+        channel: str,
+        last_id: str,
+    ) -> Iterator[tuple[str, str]]:
+        stream_id = last_id if last_id else "0"
+
+        while True:
+            # Continuosly poll for new messages,
+            # sleeping for 1s if no messages are present.
+            events = self.redis_client.xread({channel: stream_id}, block=1000, count=10)
+            for _, es in events:
+                for e in es:
+                    stream_id = e[0].decode()
+
+                    if b"message" not in e[1]:
+                        logging.warning("Malformed message, skipping")
                         continue
 
                     message = e[1][b"message"].decode()
