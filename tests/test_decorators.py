4eb707931fb33c6dc6d5817201721420001fb2b5
nursultan-berdiev <dominguez120205024@gmail.com>
2024-01-10 01:20:59 +0600

    new version

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,14 +3,11 @@ import pytest
 from odevlib.models.errors import Error
 from odevlib.utils.users import get_system_user
 from test_app.models import ExampleOModel
-from odevlib.decorators.error_atomicity import odevlib_transaction_atomic
+from odevlib.decorators.transaction import odevlib_atomic
 
 
-@odevlib_transaction_atomic
 def function_that_does_some_stuff_and_returns_error(error: bool) -> ExampleOModel | Error:
-    first_instanse = ExampleOModel(
-        test_field="Test field 1",
-    )
+    first_instanse = ExampleOModel(test_field="Test field 1")
     first_instanse.save(user=get_system_user())
     if error:
         return Error(
@@ -18,9 +15,7 @@ def function_that_does_some_stuff_and_returns_error(error: bool) -> ExampleOMode
             ui_description="Some error ui description",
             error_code=1,
         )
-    second_instanse = ExampleOModel(
-        test_field="Test field 2",
-    )
+    second_instanse = ExampleOModel(test_field="Test field 2")
     second_instanse.save(user=get_system_user())
     return second_instanse
 
@@ -29,7 +24,11 @@ def function_that_does_some_stuff_and_returns_error(error: bool) -> ExampleOMode
 def test_decorator_no_error():
     assert ExampleOModel.objects.count() == 0
 
-    result = function_that_does_some_stuff_and_returns_error(error=False)
+    @odevlib_atomic
+    def function(error: bool) -> ExampleOModel | Error:
+        return function_that_does_some_stuff_and_returns_error(error)
+
+    result = function(error=False)
     assert ExampleOModel.objects.count() == 2
     assert isinstance(result, ExampleOModel)
     assert result.test_field == "Test field 2"
@@ -39,7 +38,41 @@ def test_decorator_no_error():
 def test_decorator_with_error():
     assert ExampleOModel.objects.count() == 0
 
-    result = function_that_does_some_stuff_and_returns_error(error=True)
+    @odevlib_atomic
+    def function(error: bool) -> ExampleOModel | Error:
+        return function_that_does_some_stuff_and_returns_error(error)
+
+    result = function(error=True)
+    assert ExampleOModel.objects.count() == 0
+    assert isinstance(result, Error)
+    assert result.eng_description == "Some error eng description"
+    assert result.ui_description == "Some error ui description"
+    assert result.error_code == 1
+
+
+@pytest.mark.django_db
+def test_as_context_manager_no_error():
+    assert ExampleOModel.objects.count() == 0
+
+    with odevlib_atomic():
+        result = function_that_does_some_stuff_and_returns_error(error=False)
+
+    assert ExampleOModel.objects.count() == 2
+    assert isinstance(result, ExampleOModel)
+    assert result.test_field == "Test field 2"
+
+
+@pytest.mark.django_db
+def test_as_context_manager_with_error():
+    assert ExampleOModel.objects.count() == 0
+
+    try:
+        with odevlib_atomic():
+            result = function_that_does_some_stuff_and_returns_error(error=True)
+            raise Exception("Some exception")
+    except Exception:
+        print("Ouch! Unexpected exception! Don't worry, all transactions were rolled back!")
+
     assert ExampleOModel.objects.count() == 0
     assert isinstance(result, Error)
     assert result.eng_description == "Some error eng description"
